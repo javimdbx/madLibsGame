b195a8714765ad39d9f2467fd8222646edb9a9b5
nor1vin <nor1vin.code@gmail.com>
2025-08-08 13:13:03 +0100

    ADDED COMMENT TO TEST IF REPO WORKS 13:12  08/08/2025

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 
+/*
+       THIS IS FIRST MINI PRACTICE MINI PROJECT
+       THIS GAME CALLED MAD LIBS
+       AND I MADE IT TO PRACTICE USER INPUTS
+ */
+
 public class Main {
     public static void main(String[] args) {
+
+
+
+
+
     }
 }
